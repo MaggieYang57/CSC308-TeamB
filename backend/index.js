71423b0480c81da139ad4578e6852f853ca8e66b
Baagi <secondo@Bats-macbook.local>
2021-02-12 07:35:31 -0800

    Installed Nodemon. Use Nodemon for the npm start script.

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const env = require('dotenv');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bodyParser = require('body-parser')
 const express = require('express');
 const app = express();
 env.config();
+const Hikes  = require('./trails-schema.js')
+app.use(bodyParser.json())
 
 const uri = process.env.DB_LINK;
 if (!uri) {
@@ -12,15 +15,28 @@ if (!uri) {
 }
 mongoose.connect(uri, {
    useNewUrlParser: true,
-   useUnifiedTopology: true
+   useUnifiedTopology: true,
+   useCreateIndex: true
 })
 .then(() => {
    console.log('Atlas Connected..')
 })
 .catch(err => console.log(err));
 
+
+
+const getAllHikes = async () => {
+  return Hikes.find({})
+}
+
 app.get('/', (req, res) => {
   res.send('Hello world!');
 })
+
+app.get('/api/hike', async (req, res) => {
+  res.status(200)  
+  let hike = await getAllHikes()
+  res.send(hike)
+} )
 
 app.listen(3001);
