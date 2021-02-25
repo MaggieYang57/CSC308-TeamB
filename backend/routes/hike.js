466acd70efaaffbd723ba3743b7c3fd70054837c
Baagi <secondo@Bats-macbook.local>
2021-02-25 02:46:24 -0800

    updated the post request to match the new trails schema

--- backend/routes/hike.js
+++ backend/routes/hike.js
@@ -7,72 +7,69 @@ var ObjectID = require('mongodb').ObjectID;
 
 router.get('/testing', (req, res) => {
    res.send('List of all hikes!');
- })
+})
 
 
 
 router.get('/:id/reviews', (req, res) => {
    res.send('Get review for each hike page!');
- })
+})
 
 router.get('/:id/reviews', (req, res) => {
    res.send('Get review for each hike page!');
- })
+})
 
- router.post('/:id/reviews', (req, res) => {
+router.post('/:id/reviews', (req, res) => {
    console.log(req.body);
    res.send('Post a review on this hike page');
- })
+})
 
 
- //testing GET all hikes
+//testing GET all hikes
 
- router.get('/', async (req, res) => {
-    try {
-       const hikes = await Trail.find({});
-       res.json(hikes);
-    }
-    catch(err){
-      res.json({message:err});
-    }
+router.get('/', async (req, res) => {
+   try {
+      const hikes = await Trail.find({});
+      res.json(hikes);
+   }
+   catch (err) {
+      res.json({ message: err });
+   }
 });
- 
+
 //testing GET individual hike
 router.get('/:id', async (req, res) => {
    try {
-      const hikes = await Trail.find({"_id": req.params.id});
+      const hikes = await Trail.find({ "_id": req.params.id });
       res.json(hikes);
    }
-   catch(err){
-     res.json({message:err});
+   catch (err) {
+      res.json({ message: err });
    }
 });
 
-
-
-
- //testing Post request
-
- router.post('/', (req, res) => {
+// Post request to add a review 
+router.post('/', (req, res) => {
    const hike = new Trail({
       title: req.body.title,
+      location: req.body.location,
       description: req.body.description,
       difficulty: req.body.difficulty,
-      length: req.body.length,
-      elevation_gain: req.body.elevation_gain,
-      route_type: req.body.route_type,
+      //length: req.body.length,
+      //elevation_gain: req.body.elevation_gain,
+      //route_type: req.body.route_type,
       rating: req.body.rating,
       reviews: req.body.reviews,
       tags: req.body.tags
    });
-   
+
    hike.save()
       .then(data => {
          res.json(data);
       })
       .catch(err => {
-         res.json({message: 'failed'});
+         res.json({ message: 'failed' });
       });
- });
+});
 
- module.exports = router;
+module.exports = router;
