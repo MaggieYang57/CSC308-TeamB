6f5bacb78a8d9f15d020fcd9d2f15ba270ec3036
Sarah Rietkerk <sarahnrietkerk@gmail.com>
2021-02-25 22:18:13 -0800

    updated the database after modifying the json objects and schema, started thinking about the review schema. The updated json objects are now stored in trails3

--- backend/models/trails-schema.js
+++ backend/models/trails-schema.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose');
-const Float = require('mongoose-float').loadType(mongoose, 2);
 
 const types = mongoose.Schema.Types;
 const trailSchema = mongoose.Schema({
     title: {type: String, required: true},
     location: {type: String, required: true},
     description: {type: String, required: true},
-    difficulty: {type: String, required: true},
+    difficulty: {type: Number, required: true},
     // length: {type: String, required: false},
     // elevation_gain: {type: String, required: false},
     // route_type: {type: String, required: false},
-    rating: {type: Float, required: true},
-    reviews: {type: [String], required: false},
-    tags: {type: [String], required: true},
+    rating: {type: Number, required: true},
+    reviews: [{type: types.ObjectId, ref: 'Review', required: false}],
+    dog_friendly: {type: Boolean, required: true},
+    horseback_riding: {type: Boolean, required: true},
+    biking: {type: Boolean, required: true},
+    bird_watching: {type: Boolean, required: true},
+    family_friendly: {type: Boolean, required: true},
 
 })
 
@@ -21,4 +24,4 @@ const trailSchema = mongoose.Schema({
 
 
 //module.exports = mongoose.model('hike_data', trailSchema);
-module.exports = trailSchema
+module.exports = trailSchema;
