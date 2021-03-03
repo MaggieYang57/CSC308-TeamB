36ad7d78c4055f78ba71482bc98338b4e0dc17bd
Sarah Rietkerk <sarahnrietkerk@gmail.com>
2021-03-03 01:05:15 -0800

    Changed the filter buttons to look more like I was wanting to.

--- frontend/src/components/FilterButton.js
+++ frontend/src/components/FilterButton.js
@@ -1,69 +1,19 @@
 import React, { Component, useState, useEffect } from "react";
 import axios from "axios";
 import Table from "./Table";
-import "../css/filterBar.css";
+import "../css/filterButton.css";
 
 
 function FilterButton(props) {
-  const filterType = String(props.filterType);
-  const [message, setMessage] = useState([]);
-  async function fetchAll() {
-    try {
-      const response = await axios.get("http://localhost:3001/hike");
-      return response.data;
-    } catch (error) {
-      //We're not handling errors. Just logging into the console.
-      console.log(error);
-      return false;
-    }
-  }
+  return (
+    <div className="filterButton">
+      <label>
+        <input type="checkbox" value="1" />
+        <span>{props.name}</span>
+      </label>
+    </div>
 
-  useEffect(() => {
-    fetchAll().then((result) => {
-      if (result) setMessage(result);
-    });
-  }, []);
-
-
-  async function fetchFiltered() {
-    try {
-      const response = await axios.get("http://localhost:3001/hike/" + filter);
-      return response.data;
-    } catch (error) {
-      //We're not handling errors. Just logging into the console.
-      console.log(error);
-      return false;
-    }
-  }
-
-  const [hikes, setHikes] = useState(message);
-  const [filter, setFilter] = useState("all");
-  const handlefilterChange = (e, filterType) => {
-    switch (filterType) {
-      case "dog-friendly":
-        setFilter(e.target.name);
-        break;
-      case "bike-riding":
-        setFilter(e.target.name);
-        break;
-      case "horseback-riding":
-        setFilter(e.target.name);
-        break;
-      case "family-friendly":
-        setFilter(e.target.name);
-        break;
-      default: break;
-    }
-  };
-
-  useEffect(() => {
-    fetchFiltered().then(result => {
-      if (result)
-        setHikes(result)
-    });
-  }, [filter])
-
-  return <button onClick={() => handlefilterChange(e, {filterType})}>{props.name}</button>
+  )
 }
 
 export default FilterButton;
