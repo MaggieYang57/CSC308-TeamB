b5c11e27fe5ed6d51e63ebe090bcfc3ec3ef907d
MaggieYang57 <myang57@calpoly.edu>
2021-03-10 10:47:27 -0800

    Updated nav logo

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -7,8 +7,8 @@ export function Navigation({}) {
 
 			<nav class="navbar navbar-dark " style={{backgroundColor: '#2c6674'}}>
 			  <a class="navbar-brand" href="/">
-			  	<form class="form-inline " style = {{marginLeft: '-10vw'}} >
-			  	  <img class=" nav-link " src="/images/SH.png" style={{width: 200, height: 53}}     />
+			  	<form class="form-inline " style = {{marginLeft: '-11vw'}} >
+			  	  <img class="nav-link" src="/images/SH.png" style={{width: 200, height: 53, borderRadius: 0}}/>
 			  	  <a class="nav-link text-light" href="/Profile">Profile</a>
 				  <a class="nav-link text-light" href="/singlepage">Single</a>
 				  <a class="nav-link text-light" href="/hikeFinder">HikeFinder</a>
