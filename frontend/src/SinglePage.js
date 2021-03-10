87b0a0375fd3f2f9b7b77dbfccf7a1889c10a0b1
Michael Eirinberg <Mike@Michaels-MBP-6.attlocal.net>
2021-03-10 12:00:07 -0800

    Weather widget is now an independent class.

--- frontend/src/SinglePage.js
+++ frontend/src/SinglePage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './css/SinglePage.css';
 import moment from 'moment';
 
+import WeatherWidget from './components/WeatherWidget';
+
 const averageRatings = (ratings) => {
   let sum = 0
   for (const i in ratings)
@@ -15,33 +17,12 @@ class SinglePage extends React.Component {
     this.state = {};
   }
 
-  getWeather() {
-    fetch('http://api.openweathermap.org/data/2.5/weather?q='+this.state.location+'&units=imperial&appid=9bb2aad28619e8c45992b312cb325c36')
-  .then(res => res.json())
-  .then(data => {
-    if(data)
-    {
-     var icon = JSON.stringify(data.weather[0].icon);
-     icon = icon.replace("\"","");
-     icon = icon.replace("\"","");
-    
-     document.getElementById("weatherTempLabel").innerHTML = 'Current Temperature: ' + data.main.temp + '&#176; F <br> Feels like: ' + data.main.feels_like + '&#176; F' + '<br> Day Low: ' + data.main.temp_min + '&#176; F <br> Day High: ' + data.main.temp_max + '&#176; F' + '<br>Pressure: ' + data.main.pressure + ' hPa <br> Humidity: ' + data.main.humidity + '%';
-     document.getElementById("weatherDescription").innerHTML = data.weather[0].main + ': ' + data.weather[0].description;
-     document.getElementById("windDescription").innerHTML = 'Wind: Speed: ' + data.wind.speed + ' mph Direction: ' + data.wind.deg + '&#176;';
-     document.getElementById("sunriseSunsetDescription").innerHTML = 'Sunrise: ' + moment.unix(data.sys.sunrise).format('LT') + ' Sunset: ' + moment.unix(data.sys.sunset).format('LT');
-     document.getElementById("weatherIcon").src = 'http://openweathermap.org/img/wn/' + icon + '@2x.png'
-     document.getElementById("weatherIcon").style.margin = '0 auto';;
-     return data;
-     }
-     });
-  }
 
   componentDidMount() {
     fetch('http://localhost:3001/hike/')
       .then(res => res.json())
       .then(data => {
         this.setState({ ...data[1] });
-        this.getWeather();
       });
   }
   
@@ -75,15 +56,8 @@ class SinglePage extends React.Component {
                   <h2 id = "rating">★{this.state.rating}</h2>
                 </div>
                 <p id="desc">{this.state.description}</p>
-                <br></br>     
-                <div className = "boxed">
-                  <h3>Current Weather Conditions</h3>
-                  <img id="weatherIcon" height="100" />
-                  <p id="weatherTempLabel"></p>
-                  <p id="weatherDescription"></p>
-                  <p id="windDescription"></p>
-                  <p id="sunriseSunsetDescription"></p>
-                </div>
+                <br></br>
+                <WeatherWidget city={this.state.location}/>     
                 <br></br>
                 <p>{this.state.tags}</p>
             </div>
