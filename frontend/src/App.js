1ce64a619b7b9bfac5a6539bd22e83dc6db7d05f
Baagi <secondo@Bats-macbook.local>
2021-03-13 18:15:35 -0800

    Revert "Change navigation links and divide hikefinder page into 2 pages."
    This reverts commit 9e2abe7854427ff7d775bf75c55ec6d3c7f7d9f7.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,115 +16,115 @@ import FilterBar from './components/FilterBar';
 import Table from './components/Table';
 
 function App() {
-   const [appState, setAppState] = useState({
-      baseHikeData: [],
-      filteredDataIndexes: new Set(),
-      filters: new Set(),
-   });
+  const [appState, setAppState] = useState({
+    baseHikeData: [],
+    filteredDataIndexes: new Set(),
+    filters: new Set(),
+  });
 
-   async function fetchAll() {
-      try {
-         const response = await axios.get("http://localhost:3001/hike");
-         return response.data;
-      } catch (error) {
-         //We're not handling errors. Just logging into the console.
-         console.log(error);
-         return false;
+  async function fetchAll() {
+    try {
+      const response = await axios.get("http://localhost:3001/hike");
+      return response.data;
+    } catch (error) {
+      //We're not handling errors. Just logging into the console.
+      console.log(error);
+      return false;
+    }
+  }
+
+  useEffect(() => {
+    fetchAll().then((result) => {
+      if (result) setAppState({ filteredDataIndexes: new Set([...Array(result.length).keys()]), filters: appState.filters, baseHikeData: result })
+    });
+  }, []);
+
+  function addFilter(filter) {
+    const hikeData = appState.baseHikeData;
+    const filters = new Set([...appState.filters]);
+    filters.add(filter);
+    let indices = new Set([...appState.filteredDataIndexes]);
+    if (filters.size === 1) {
+      indices = new Set()
+    }
+    for (var i = 0; i < hikeData.length; i++) {
+      if (hikeData[i][filter] === true) {
+        indices.add(i);
       }
-   }
+    }
 
-   useEffect(() => {
-      fetchAll().then((result) => {
-         if (result) setAppState({ filteredDataIndexes: new Set([...Array(result.length).keys()]), filters: appState.filters, baseHikeData: result })
-      });
-   }, []);
+    setAppState({
+      filteredDataIndexes: indices,
+      filters,
+      baseHikeData: appState.baseHikeData,
+    });
+  }
 
-   function addFilter(filter) {
-      const hikeData = appState.baseHikeData;
-      const filters = new Set([...appState.filters]);
-      filters.add(filter);
-      let indices = new Set([...appState.filteredDataIndexes]);
-      if (filters.size === 1) {
-         indices = new Set()
-      }
+  function removeFilter(filter) {
+    const hikeData = appState.baseHikeData;
+    const filters = new Set([...appState.filters]);
+    filters.delete(filter);
+    let indices = new Set();
+    if (filters.size > 0) {
       for (var i = 0; i < hikeData.length; i++) {
-         if (hikeData[i][filter] === true) {
+        for (const filterOpt of filters) {
+          if (hikeData[i][filterOpt] === true) {
             indices.add(i);
-         }
+          }
+        }
       }
+  }
 
-      setAppState({
-         filteredDataIndexes: indices,
-         filters,
-         baseHikeData: appState.baseHikeData,
-      });
-   }
+    else {
+      indices = new Set([...Array(appState.baseHikeData.length).keys()]);
+    }
 
-   function removeFilter(filter) {
-      const hikeData = appState.baseHikeData;
-      const filters = new Set([...appState.filters]);
-      filters.delete(filter);
-      let indices = new Set();
-      if (filters.size > 0) {
-         for (var i = 0; i < hikeData.length; i++) {
-            for (const filterOpt of filters) {
-               if (hikeData[i][filterOpt] === true) {
-                  indices.add(i);
-               }
-            }
-         }
-      }
+    setAppState({
+      filteredDataIndexes: indices,
+      filters: filters,
+      baseHikeData: appState.baseHikeData,
+    });
+  }
 
-      else {
-         indices = new Set([...Array(appState.baseHikeData.length).keys()]);
-      }
+  function handleFilterChange(newFilter) {
+    if (appState.filters.has(newFilter)) {
+      removeFilter(newFilter);
+    } else {
+      addFilter(newFilter);
+    }
+  }
 
-      setAppState({
-         filteredDataIndexes: indices,
-         filters: filters,
-         baseHikeData: appState.baseHikeData,
-      });
-   }
+  return (
+    <BrowserRouter>
+      <title>SLO Hikes</title>
+      <div className="App" style={{margin: "auto"}}>
+        <Navigation />
+        <Switch>
+          <Route exact path ="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/hike">Test</Route>
 
-   function handleFilterChange(newFilter) {
-      if (appState.filters.has(newFilter)) {
-         removeFilter(newFilter);
-      } else {
-         addFilter(newFilter);
-      }
-   }
-
-   return (
-      <BrowserRouter>
-         <title>SLO Hikes</title>
-         <div className="App" style={{ margin: "auto" }}>
-            <Navigation />
-            <Switch>
-               <Route exact path="/">
-                  <HomePage />
-               </Route>
-               <Route exact path="/hike">Test</Route>
-
-               <Route exact path="/hike/:id" component={SinglePage} />
-
-               <Route exact path="/hikeFinder">
-                  <HikeFinder />
-               </Route>
-
-               <Route exact path="/searchHikes">
-                  <FilterBar onChange={handleFilterChange} />
-                  <Table hikeList={appState.baseHikeData.filter((_, i) => appState.filteredDataIndexes.has(i))} />
-               </Route>
-               <Route exact path="/login">
-                  <Login />
-               </Route>
-               <Route exact path="/signup">
-                  <Signup />
-               </Route>
-            </Switch>
-         </div>
-      </BrowserRouter>
-   );
+          <Route exact path="/hike/:id" component={SinglePage} />          
+          
+          <Route exact path="/singlepage">
+            <SinglePage />
+          </Route>
+          <Route exact path="/hikeFinder">
+            <FilterBar onChange={handleFilterChange}/>
+            <Table hikeList={appState.baseHikeData.filter((_, i) => appState.filteredDataIndexes.has(i))} />
+            <HikeFinder />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
