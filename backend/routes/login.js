b31561c1d2ff4fd316845e05618b41ecd8e410d0
MaggieYang57 <myang57@calpoly.edu>
2021-04-19 10:47:33 -0700

    eslint and prettier for backend

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,40 +1,36 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const router = express.Router();
+const mongoose = require('mongoose')
+const express = require('express')
+const router = express.Router()
 
-const userSchema = require('../models/users-schema');
-const User = mongoose.model('User', userSchema, 'Users');
+const userSchema = require('../models/users-schema')
+const User = mongoose.model('User', userSchema, 'Users')
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
 
 router.post('/', async (req, res) => {
-    const {email, password, user_type} = req.body
-    if (user_type == null) {
-       res.status(404).send("Invalid user type") 
+  const { email, password, userType } = req.body
+  if (userType == null) {
+    res.status(404).send('Invalid user type')
+  }
+
+  User.findOne({ user_email: email }).then(function (result) {
+    if (!result) {
+      console.log('Invalid email')
+      res.status(404).send('Invalid email')
+    } else {
+      const userPassword = result.password
+      const valid = bcrypt.compareSync(password, userPassword)
+      if (valid) {
+        console.log('login successful')
+        res.send(result)
+      } else {
+        console.log('Invalid password')
+        res.status(404).send('Invalid password')
+      }
     }
- 
-    User.findOne({'user_email': email}).then(function(result) {
-        if (!result) {
-            console.log("Invalid email")
-            res.status(404).send("Invalid email")
-        }
-       else {
-            let userPassword = result.password
-            const valid = bcrypt.compareSync(password, userPassword);
-            if (valid) 
-            {
-                console.log("login successful")
-                res.send(result)
-            } 
-            else 
-            {
-                console.log("Invalid password")
-                res.status(404).send("Invalid password")   
-            }  
-        }
-    })
- });
+  })
+})
 
- //reset password
+// reset password
 
- module.exports = router;
+module.exports = router
